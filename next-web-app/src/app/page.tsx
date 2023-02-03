2eac1cb7f827a40083648e55c2a6723d5c24ad3a
Angelo <vageez@gmail.com>
2023-02-03 14:05:48 -0500

    Adding copy

--- next-web-app/src/app/page.tsx
+++ next-web-app/src/app/page.tsx
@@ -2,10 +2,79 @@
 export default function Home() {
   return (
     <>
-      <header className="m-10">
+      <header className="m-10 b-1">
         <h1>Ilios Token</h1>
       </header>
-      <main className="m-10">{/* <TokenManager /> */}</main>
+      <main className="m-10">
+        <ul className="list-disc md:list-disc">
+          <li>
+            Token for Pre-IPO Swap of Ilios ESG Labs (working name): Ilios Token
+            will serve as a pre-IPO swap option for the Ilios ESG Labs (working
+            name). This will allow investors to exchange their current holdings
+            for Ilios Tokens and gain early access to the potential benefits of
+            the Ilios ESG Labs. The use of a token for this purpose provides a
+            convenient, efficient and secure way for investors to participate in
+            the pre-IPO process.
+          </li>
+          <li>
+            Token as Incentive for Green Initiatives: Ilios Token will serve as
+            an incentive for green initiatives through cooperation with
+            governments. By using Ilios Tokens as a reward for participating in
+            green initiatives, the token will play a crucial role in driving the
+            growth and adoption of sustainable and environmentally conscious
+            practices. This will not only benefit the environment but also
+            encourage wider adoption and investment in green initiatives.
+          </li>
+          <li>
+            Token Swap for ESG Fund Units: Ilios Token will also serve as a
+            token swap for ESG fund units investing fiat currency in green ESG
+            projects. Investors who want to invest in sustainable and
+            environmentally conscious initiatives can use Ilios Tokens to
+            purchase ESG fund units. This will allow them to invest in green
+            initiatives while taking advantage of the benefits and security that
+            come with using a crypto token.
+          </li>
+          <li>
+            Limited NFT Launch for Current Ilios Token Owners: Ilios Token will
+            also have a limited NFT launch for current Ilios Token owners. This
+            will allow current owners to access unique and collectible NFTs,
+            providing them with additional benefits and opportunities for
+            engagement with the Ilios ecosystem. The limited NFT launch will
+            also help to drive the growth and adoption of Ilios Tokens by
+            offering current owners a new way to participate in and benefit from
+            the ecosystem.
+          </li>
+          <li>
+            Staking with Interesting APY: Ilios Token will also offer staking
+            with an attractive APY of over 5%. This will provide a way for
+            investors to earn passive income from their holdings, incentivizing
+            long-term investment and contributing to the stability of the Ilios
+            ecosystem. The attractive APY will also make Ilios Tokens an
+            attractive option for those looking for alternative investment
+            opportunities in the crypto space.
+          </li>
+          <li>
+            Ilios ESG Exchange for Green Initiative Alt-Tokens: Ilios Token will
+            host and trade on the Ilios ESG Exchange, a platform specifically
+            designed for green initiative alt-tokens. This will provide a
+            convenient and secure platform for trading green initiative
+            alt-tokens and will help to drive the growth and adoption of
+            sustainable and environmentally conscious initiatives in the crypto
+            space. The Ilios ESG Exchange will also serve as a hub for
+            information and resources on sustainable and environmentally
+            conscious initiatives, further driving their growth and adoption.
+          </li>
+          Mint for Service New Token Projects in the ESG Space: Ilios Token will
+          also offer a minting service for new token projects in the ESG space.
+          This will allow new projects to benefit from the security, efficiency
+          and convenience that come with using a crypto token, and will help to
+          drive the growth and adoption of sustainable and environmentally
+          conscious initiatives in the crypto space. The minting service will
+          also provide new token projects with access to the resources and
+          support offered by the Ilios ecosystem, further contributing to their
+          growth and success.
+        </ul>
+      </main>
       <footer></footer>
     </>
   )
